31aeb1fec020bb1fca7bb26fc9a30d0725274f7c
이해인 <ihaein@ihaein-ui-MacBookPro.local>
2023-02-22 12:59:40 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,12 +2,29 @@ import React from "react";
 import "./Introduce.scss";
 
 const Introduce = () => {
+  const handleInput = () => {
+    console.log("input태그 동작");
+  };
+  const handleButton = () => {
+    console.log("botton 태그 동작");
+  };
+
   return (
     <div className="introduceWrap">
       <img src="./images/HeinImg.jpg" className="introduce-img" alt="HeinImg" />
       <p className="introduce-text">
         <span className="introduce-name">이해인</span>42기
       </p>
+      <div className="introduce-input-wrap">
+        <input
+          className="introduce-input"
+          onChange={handleInput}
+          type="text"
+        ></input>
+        <button onClick={handleButton} className="input-btn">
+          클릭
+        </button>
+      </div>
     </div>
   );
 };
