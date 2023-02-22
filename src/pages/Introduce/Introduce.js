59e6429c16aaea00f3204e20aafa097488f85395
soomminglee <soomming.lee@gmail.com>
2023-02-22 13:01:46 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,7 +1,14 @@
-import React from "react";
 import "./Introduce.scss";
 
 export default function Introduce() {
+  const HandleInput = (evnet) => {
+    console.log("input 태그 동작");
+  };
+
+  const HandleButton = (event) => {
+    console.log("button 태그 동작");
+  };
+
   return (
     <div className="introduce-wrapper">
       <div className="introduce-box">
@@ -10,8 +17,16 @@ export default function Introduce() {
           src="https://images.unsplash.com/photo-1503435980610-a51f3ddfee50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
           alt="profile-img"
         ></img>
-        <h1 className="name">이수민</h1>
-        <p className="generation">42기</p>
+        <div className="information">
+          <h1 className="name">이수민</h1>
+          <p className="generation">42기</p>
+        </div>
+        <div>
+          <input type="text" onChange={HandleInput} />
+          <button type="submit" onClick={HandleButton}>
+            클릭
+          </button>
+        </div>
       </div>
     </div>
   );
