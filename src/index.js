9f439d5dbee0b9aa8ba98e6b119d2a7453e7e5f0
이유진 <yjoct13@gmail.com>
2023-02-23 10:33:29 +0900

    Assignment3-1 : Color 컴포넌트 구현

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Introduce from "./pages/Introduce/Introduce";
 import Review from "./pages/Product/components/Review/Review";
+import Color from "./pages/Product/components/Color/Color";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Review />);
+root.render(<Color />);
