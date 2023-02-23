e5ddf6dcc0da1cc05589d7fa19f484beeb0b6daf
이유진 <yjoct13@gmail.com>
2023-02-23 11:10:28 +0900

    Assignment3-2 : Count 컴포넌트 구현

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Introduce from "./pages/Introduce/Introduce";
 import Review from "./pages/Product/components/Review/Review";
 import Color from "./pages/Product/components/Color/Color";
+import Count from "./pages/Product/components/Count/Count";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Color />);
+root.render(<Count />);
